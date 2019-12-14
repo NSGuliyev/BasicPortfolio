de4c2370d66b6e3b5a3d7e031bb6384847d69d0e
NSGuliyev <nesimi018@yahoo.com>
2019-12-14 18:45:30 -0500

    JavaScript update

--- script.js
+++ script.js
@@ -7,17 +7,41 @@ $(document).ready(function (){
     console.log("The current hour is " + moment().hour());
 
     $("#exact-time").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    // changeColor();
+
+
+
 
     setInterval(function () {
         $("#exact-time").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
     }, 1000);
 
 
+    for (var i = 9; i < 17; i++) {
+        $('#' + 1).val(localStorage.getItem("hour " + 1 ));
+    }
+
+
     saveButton.click(function () {
         event.preventDefault();
         console.log("I've been clicked");
-        
-    })
+
+
+        var a = this;
+        var results = a.previousElementSibling.firstElementChild;
+        console.log($(results));
+
+        data = $(results).val().trim();
+        console.log(data);
+
+        var time = results.id;
+        console.log("hour " + time);
+
+        localStorage.setItem("hour " + time,data);
+
+    });
+
+    
 
 
 
